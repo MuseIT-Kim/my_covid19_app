28e0660d7094799b74d16466ca68d60933801d09
kim <kim@museit.co.jp>
2020-05-19 01:03:41 +0900

    [create]RestApi&Json tutorial

---
+++ my_covid19_app/my_covid19_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  my_covid19_app
+//
+//  Created by 金相恩 on 2020/05/19.
+//  Copyright © 2020 Kim. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
