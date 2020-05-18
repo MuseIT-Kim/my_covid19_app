94e56f3d86dbded7005a7426d9848e4283336015
kim <kim@museit.co.jp>
2020-05-19 01:40:30 +0900

    [add]install pod&new file

---
+++ my_covid19_app/my_covid19_app/CovidFetchRequest.swift
@@ -0,0 +1,13 @@
+//
+//  CovidFetchRequest.swift
+//  my_covid19_app
+//
+//  Created by 金相恩 on 2020/05/19.
+//  Copyright © 2020 Kim. All rights reserved.
+//
+import Alamofire
+import SwiftyJSON
+import Foundation
+class CovidFetchRequest : ObservableObject {
+    
+}
